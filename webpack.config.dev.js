1921c445a7d0d15a790b101fb16d5348df2644a9
Jimmy Vasquez <jimmy.vasquez22@gmail.com>
2021-05-27 10:06:41 -0500

    plantilla react

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,9 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
-const TerserPlugin = require('terser-webpack-plugin');
-const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 
 
 module.exports = {
@@ -11,16 +8,11 @@ module.exports = {
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
-    publicPath: "/",  
   },
   resolve: {
-    extensions: ['.js', '.jsx'],
-    alias: {
-      '@components': path.resolve(__dirname, 'src/components/'),
-      '@styles':  path.resolve(__dirname, 'src/styles/'),
-    }
+    extensions: ['.js', '.jsx']
   },
-  mode: 'production',
+  mode: development,
   module: {
     rules: [
       {
@@ -54,13 +46,11 @@ module.exports = {
     new MiniCssExtractPlugin({
       filename: '[name].css'
     }),
-    new CleanWebpackPlugin(),
   ],
-  optimization: {
-    minimize: true,
-    minimizer: [
-      new CssMinimizerPlugin(),
-      new TerserPlugin()
-    ]
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),
+    compress: true,
+    port: 3006,
+    open: true
   }
 }
